15d70b6d2d9a9b1f74cc551cb9c3e2d2883bbe63
gogafefe <120042683+gogafefe@users.noreply.github.com>
2023-02-23 00:25:04 +0300

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,36 +1,40 @@
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((1300,800))
+win = pygame.display.set_mode((1024, 576))
 pygame.display.set_caption('Марио')
-player = pygame.image.load('')
-bg = pygame.image.load('')
+player = pygame.image.load('sprite.png')
+bg = pygame.image.load('bg.png')
 
 x = 50
-y = 50
+y = 370
 
 
+speed = 4
+yupst = 200
+yup = 200
 
-speed = 5
 
 clock = pygame.time.Clock()
-run=True
-while(run):
+run = True
+while run:
     clock.tick(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    elif keys[pygame.K_RIGHT]:
-        x += speed
-    elif keys[pygame.K_UP]:
+
+    if y <  yup and yup < 370:
         y += speed
-    elif keys[pygame.K_DOWN]:
-        x -= speed
+        yup += speed
+    elif yup >= 370:
+        yup = yupst
+    elif keys[pygame.K_UP] or keys[pygame.K_SPACE]:
+        y -= speed
+    elif y < 370:
+        y += speed
 
     win.blit(bg, (0, 0))
-    win.blit(player,(50,50))
+    win.blit(player, (50, y))
     pygame.display.update()
 pygame.quit()
