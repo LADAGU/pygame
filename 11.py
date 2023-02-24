e95c17c5d5ed713ffa2b7178a7a540442729d0c9
gogafefe <120042683+gogafefe@users.noreply.github.com>
2023-02-24 21:10:12 +0300

    11_03.py
    now with sprites

--- 11.py
+++ 11.py
@@ -1,36 +1,64 @@
+import os
+import random
 import pygame
 
-pygame.init()
-win = pygame.display.set_mode((1300,800))
-pygame.display.set_caption('Марио')
-player = pygame.image.load('')
-bg = pygame.image.load('')
 
-x = 50
-y = 50
+if __name__ == '__main__':
+    pygame.init()
 
+    bg = pygame.image.load('bg.png')
+    box = pygame.image.load('data/wall.png')
+    wall = pygame.transform.scale(box, (50, 50))
+    screen = pygame.display.set_mode((bg.get_width(), bg.get_height()))
+    pygame.display.set_caption('Марио')
 
+    def load_image(name, colorkey=None):
+        fullname = os.path.join('data', name)
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
 
-speed = 5
+    players = pygame.sprite.Group()
+    player = pygame.sprite.Sprite()
+    player_image = load_image('sprite.png')
+    player.image = pygame.transform.scale(player_image, (50, 70))
+    player.rect = player.image.get_rect()
+    players.add(player)
+    player.rect.x = 50
+    player.rect.y = 380
 
-clock = pygame.time.Clock()
-run=True
-while(run):
-    clock.tick(50)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    elif keys[pygame.K_RIGHT]:
-        x += speed
-    elif keys[pygame.K_UP]:
-        y += speed
-    elif keys[pygame.K_DOWN]:
-        x -= speed
+    y = 380
 
-    win.blit(bg, (0, 0))
-    win.blit(player,(50,50))
-    pygame.display.update()
-pygame.quit()
+    speed = 4
+    is_jump = False
+    yspeed = 0
+
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(50)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        keys = pygame.key.get_pressed()
+
+        if is_jump:
+            y -= yspeed
+            yspeed -= 2
+        elif keys[pygame.K_UP] or keys[pygame.K_SPACE]:
+            is_jump = True
+            yspeed = 24
+        if y > 380:
+            is_jump = False
+            yspeed = 0
+            y = 380
+
+        screen.blit(bg, (0, 0))
+        players.draw(screen)
+        player.rect.y = y
+        pygame.display.update()
+    pygame.quit()
