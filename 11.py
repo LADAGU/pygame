c148227b204b3f32f492133c2cdead31c620d2d0
gogafefe <120042683+gogafefe@users.noreply.github.com>
2023-02-24 12:12:55 +0300

    11_02.py
    new jump func

--- 11.py
+++ 11.py
@@ -1,36 +1,41 @@
 import pygame
 
 pygame.init()
-win = pygame.display.set_mode((1300,800))
+win = pygame.display.set_mode((1024, 576))
 pygame.display.set_caption('Марио')
-player = pygame.image.load('')
-bg = pygame.image.load('')
+player = pygame.image.load('sprite.png')
+bg = pygame.image.load('bg.png')
 
 x = 50
-y = 50
+y = 370
 
 
+speed = 4
+is_jump = False
+yspeed = 0
 
-speed = 5
 
 clock = pygame.time.Clock()
-run=True
-while(run):
+run = True
+while run:
     clock.tick(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        x -= speed
-    elif keys[pygame.K_RIGHT]:
-        x += speed
-    elif keys[pygame.K_UP]:
-        y += speed
-    elif keys[pygame.K_DOWN]:
-        x -= speed
+
+    if is_jump:
+        y -= yspeed
+        yspeed -= 2
+    elif keys[pygame.K_UP] or keys[pygame.K_SPACE]:
+        is_jump = True
+        yspeed = 24
+    if y > 370:
+        is_jump = False
+        yspeed = 0
+        y = 370
 
     win.blit(bg, (0, 0))
-    win.blit(player,(50,50))
+    win.blit(player, (50, y))
     pygame.display.update()
 pygame.quit()
